944cc4efbbfc7829fc8818e9ab044dd8921447c9
Hussain Abbasi <intelliabb@live.com>
2018-03-03 17:58:13 -0600

    Services

---
+++ HTracking.Services/ApiService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using HTracking.Data;
+using HTracking.Models;
+
+namespace HTracking.Services
+{
+    public interface IApiService 
+    {
+        IList<BaseForm> GetForms();
+        void SubmitForm(BaseForm form);
+    }
+
+    public class ApiService : IApiService
+    {
+        readonly IDataService _dataService;
+
+        public ApiService(IDataService dataService)
+        {
+            _dataService = dataService;
+        }
+
+        public IList<BaseForm> GetForms()
+        {
+            return _dataService.GetForms();
+        }
+
+        public void SubmitForm(BaseForm form)
+        {
+            _dataService.SubmitForm(form);
+        }
+    }
+}
