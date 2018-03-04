c93f367ffef18d2b28281b365ad828eaf0f334cb
Hussain Abbasi <intelliabb@live.com>
2018-03-03 18:24:08 -0600

    Getting forms successfully.

---
+++ HTracking/Controllers/FormsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HTracking.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HTracking.Controllers
+{
+    public class FormsController : Controller
+    {
+        readonly IApiService _apiService;
+
+        public FormsController(IApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var forms = _apiService.GetForms();
+            return View();
+        }
+    }
+}
